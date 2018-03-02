e6d14a12d5a1bc4ef97ddf9337b9f5f37740e493
DeclandHorlick <deco-9-deco@live.ie>
2018-03-02 09:06:13 +0000

    Tony and Decland C problem solve

--- src/main/webapp/app/app-state-manager.js
+++ src/main/webapp/app/app-state-manager.js
@@ -10,7 +10,23 @@
             templateUrl: "app/feature/dashboard/dashboard.html"
         }).state("account", {
                 url: "/account",
+                params: {
+			    obj: null
+			    },
                 templateUrl: "app/feature/account/account.html"
+        }).state("transaction", {
+                url: "/transaction",
+                params: {
+     			obj: null
+   				},
+                templateUrl: "app/feature/transaction/transaction.html"
+        }).state("transactionDetails", {
+                url: "/transactionDetails", 
+                params: {
+     			obj: null
+  				},
+                templateUrl: "app/feature/transactionDetails/transactionDetails.html"
         })
     });
+    
 }());
